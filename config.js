153b368354fbf394137e59b0b6e581dfa912f2a8
glitchisno1 <olamiposioladinni14@gmail.com>
2025-02-23 15:46:30 +0100

    Update config.js

--- config.js
+++ config.js
@@ -3,8 +3,8 @@ require("dotenv").config();
 
 module.exports = {
     prefix: process.env.PREFIX || ".",
-    ownerName: process.env.OWNER_NAME || "Toxxic",
-    ownerNumber: process.env.OWNER_NUMBER || "2348165846414",
+    ownerName: process.env.OWNER_NAME || "Glitch",
+    ownerNumber: process.env.OWNER_NUMBER || "23490784378422",
     mode: process.env.MODE || "private",
     region: process.env.REGION || "Nigeria",
     botName: process.env.BOT_NAME || "Rias Gremory V3",
@@ -12,9 +12,9 @@ module.exports = {
     exifAuthor: process.env.EXIF_AUTHOR || "Toxxic",
     timeZone: process.env.TIME_ZONE || "Africa/Lagos",
     presenceStatus: process.env.PRESENCE_STATUS || "unavailable",
-    autoRead: process.env.AUTO_READ === "true",
-    autoViewStatus: process.env.AUTO_VIEW_STATUS === "true",
-    autoReact: process.env.AUTO_REACT === "true",
+    autoRead: process.env.AUTO_READ === "false",
+    autoViewStatus: process.env.AUTO_VIEW_STATUS === "false",
+    autoReact: process.env.AUTO_REACT === "false",
     sessionId: process.env.SESSION_ID || "",
     autoRejectEnabled: process.env.AUTO_REJECT_ENABLED || "false",
 };
